75393f1d7220cb76e856d08e152dd985447a0820
Sergey G <jallygaming@gmail.com>
2020-03-12 15:22:52 +0200

    2

--- Items/Materials/Class1.cs
+++ Items/Materials/Class1.cs
@@ -3,10 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Terraria.ModLoader;
 
 namespace ProjectV.Items.Materials
 {
-    class Class1
+    public class Class1 : ModItem
     {
     }
 }
