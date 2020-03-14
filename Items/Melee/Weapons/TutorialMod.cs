600551f3cd0780ce5d36122b3083bd3c44dc3121
Sergey G <jallygaming@gmail.com>
2020-03-14 08:53:50 +0200

    1

--- Items/Melee/Weapons/TutorialMod.cs
+++ Items/Melee/Weapons/TutorialMod.cs
@@ -1,10 +1,7 @@
 using Terraria.ID;
 using Terraria.ModLoader;
-using Terraria;
-using Microsoft.Xna.Framework;
-using Terraria.DataStructures;
 
-namespace TutorialMod.Items
+namespace ProjectV.Items.Melee.Weapons
 {
 	public class TutorialMod : ModItem
 	{
@@ -15,39 +12,20 @@ namespace TutorialMod.Items
 		}
 
 		public override void SetDefaults()
-		{
-			item.damage = 8;
-			item.melee = true;
-			item.width = 40;
-			item.height = 40;
-			item.useTime = 12;
-			item.useAnimation = 20;
-			item.useStyle = 1;
-			item.knockBack = 6;
-			item.value = 10000;
-			item.rare = 2;
-			item.UseSound = SoundID.Item1;
-			item.autoReuse = true;
-			item.shoot = mod.ProjectileType("Tools");
-			item.shootSpeed = 10f;
-			
-
-		}
-
-		public override void AddRecipes()
-		{
-			ModRecipe recipe = new ModRecipe(mod);
-			recipe.AddIngredient(ItemID.DirtBlock, 10);
-			recipe.AddTile(TileID.WorkBenches);
-			recipe.SetResult(this);
-			recipe.AddRecipe();
-		}
-
-	
-
-		
-
-
-
-	}
+        {
+            item.damage = 10;
+            item.melee = true;
+            item.useStyle = 1;
+            item.useTurn = true;
+            item.autoReuse = true;
+            item.useAnimation = 22;
+            item.useTime = 22;
+            item.width = 40;
+            item.height = 40;
+            item.UseSound = SoundID.Item1;
+            item.knockBack = 4f;
+            item.shoot = mod.ProjectileType("Tools");
+            item.shootSpeed = 8f;
+        }
+    }
 }
--- Projectiles/Melee/Tools.cs
+++ Projectiles/Melee/Tools.cs
@@ -1,74 +1,42 @@
 ﻿using Microsoft.Xna.Framework;
-using Microsoft.Xna.Framework.Graphics;
 using Terraria;
 using Terraria.ID;
-using Terraria.Localization;
 using Terraria.ModLoader;
 
-namespace TutorialMod.Items
+namespace ProjectV.Projectiles.Melee
 {
     public class Tools : ModProjectile
     {
-        
-
-        public override Color? GetAlpha(Color lightColor) { return Color.White; }
+        public override void SetStaticDefaults()
+        {
+            Main.projFrames[projectile.type] = 8;
+        }
 
         public override void SetDefaults()
         {
-            projectile.width = 18;
-            projectile.height = 9;
-            projectile.friendly = true;
+            projectile.melee = true;
             projectile.ignoreWater = true;
-            projectile.penetrate = 5;
-            projectile.timeLeft = 250;
-            projectile.tileCollide = true;
-
-
+            projectile.tileCollide = false;
+            projectile.width = 128;
+            projectile.height = 128;
+            projectile.friendly = true;
+            projectile.penetrate = 3;
+            projectile.timeLeft = 240;
+            // projectile.aiStyle = 3;
+            // aiType = ProjectileID.Meteor1;
         }
 
-        public override void AI()
+        /*public override void AI()
         {
-            projectile.velocity.Y = projectile.velocity.Y + 0.1f; 
-            if (projectile.velocity.Y > 16f) 
-            {
-                projectile.velocity.Y = 16f;
-            }
+            // projectile.velocity.Y += projectile.velocity.Y + 0.01f; // добавляет гравитацию
+            // projectile.velocity.X = projectile.velocity.X * 0.97f; // добавляет сопротивлению ветру
+            // projectile.rotation += 0.4f * (float)projectile.direction; // вращение снаряда
+            // projectile.rotation = projectile.velocity.ToRotation(); // положение снаряда
+            // Lighting.AddLight(projectile.Center, 10f, 10f, 3f); // свечение снаряда в пикселях
 
-            projectile.ai[0] += 1f; 
-            if (projectile.ai[0] >= 15f)
-            {
-                projectile.ai[0] = 15f;
-                projectile.velocity.Y = projectile.velocity.Y + 0.1f;
-            }
-
-            if (Main.rand.Next(5) == 0) 
-            {
-
-                int choice = Main.rand.Next(3);
-                if (choice == 0) 
-                {
-                    choice = 57;
-                }
-                else if (choice == 1)
-                {
-                    choice = 57;
-                }
-                else
-                {
-                    choice = 58;
-                }
-                Vector2 projectilePosition = projectile.position;
-                projectilePosition -= projectile.velocity * 0.25f;
-                int dust = Dust.NewDust(projectilePosition, 1, 1, 76, 0f, 0f, 0, default(Color), 1f);
-            }
-
-
-
-
-            Lighting.AddLight(projectile.Center, 1f, 1f, 1f);
-            projectile.rotation = projectile.velocity.ToRotation() + MathHelper.PiOver2; 
-        }
-
-
+            //projectile.velocity.X = projectile.velocity.X * 0.985f;
+            //projectile.velocity.Y = projectile.velocity.Y * 0.985f;
+            //Dust.NewDust(projectile.position + projectile.velocity, projectile.width, projectile.height, 173, projectile.velocity.X * 0.5f, projectile.velocity.Y * 0.5f, 0, default(Color), 1f);
+        }*/
     }
 }
